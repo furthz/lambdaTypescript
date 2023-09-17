6cf631e5b631d7836e8906a6401a9fe712b1b1ee
Raul Talledo <raul.talledo@soapros.pe>
2023-09-17 17:38:47 -0500

    Initial commit

---
+++ test/01-lambda-typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 01LambdaTypescript from '../lib/01-lambda-typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/01-lambda-typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 01LambdaTypescript.01LambdaTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
